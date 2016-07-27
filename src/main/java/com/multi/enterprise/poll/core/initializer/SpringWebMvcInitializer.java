e2160e9270743a30afc300cb0d63836a00f88b46
Rohit Dhumal <Rohit@Rohits-Air.home>
2016-07-26 22:57:36 -0400

    Added spring configs

---
+++ src/main/java/com/multi/enterprise/poll/core/initializer/SpringWebMvcInitializer.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.multi.enterprise.poll.core.initializer;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.multi.enterprise.commons.jdbc.config.JdbcConfig;
+import com.multi.enterprise.poll.core.config.root.MainConfig;
+import com.multi.enterprise.poll.core.config.servlet.WebMvcConfig;
+
+/**
+ * @author Robot
+ *
+ */
+public class SpringWebMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getRootConfigClasses
+	 * ()
+	 */
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class<?>[] { MainConfig.class };
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getServletConfigClasses
+	 * ()
+	 */
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebMvcConfig.class, JdbcConfig.class };
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.web.servlet.support.AbstractDispatcherServletInitializer#getServletMappings()
+	 */
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/ws" };
+	}
+
+}
