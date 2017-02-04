8350415a1943b58dcbcd00208a6f01d451e8b651
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-02-04 15:12:51 -0800

    Added questions dao functinality Added health controller

--- src/main/java/com/multi/enterprise/poll/core/dao/mapper/QuestionMapper.java
+++ src/main/java/com/multi/enterprise/poll/core/dao/mapper/QuestionMapper.java
@@ -3,9 +3,14 @@
  */
 package com.multi.enterprise.poll.core.dao.mapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
 
 import com.multi.enterprise.commons.jdbc.dao.mapper.BaseRowMapper;
+import com.multi.enterprise.types.poll.Options;
 import com.multi.enterprise.types.poll.Question;
 
 /**
@@ -14,5 +19,19 @@ import com.multi.enterprise.types.poll.Question;
  */
 @Repository
 public class QuestionMapper extends BaseRowMapper<Question> {
+
+	@Override
+	public Question mapRow(final ResultSet resultSet, final int rowNum) throws SQLException {
+		final Question question = new Question();
+		question.setId(resultSet.getString("id"));
+		question.setUserId(resultSet.getString("userId"));
+		question.setQuestion(resultSet.getString("question"));
+		question.setOptionType(resultSet.getString("option_type"));
+		question.setQrCodeUrl(resultSet.getString("qr_code_url"));
+		question.setMediaUrl(resultSet.getString("media_url"));
+		question.setTotalVotes(resultSet.getInt("total_votes"));
+		question.setOpitons(new ArrayList<Options>());
+		return question;
+	}
 
 }
--- src/main/java/com/multi/enterprise/poll/core/dao/mapper/QuestionSqlParameterMapper.java
+++ src/main/java/com/multi/enterprise/poll/core/dao/mapper/QuestionSqlParameterMapper.java
@@ -3,8 +3,11 @@
  */
 package com.multi.enterprise.poll.core.dao.mapper;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Repository;
 
+import com.multi.enterprise.commons.jdbc.dao.OrderedMapSqlParameterSource;
 import com.multi.enterprise.commons.jdbc.dao.mapper.BaseSqlParameterMapper;
 import com.multi.enterprise.types.poll.Question;
 
@@ -31,6 +34,22 @@ public class QuestionSqlParameterMapper extends BaseSqlParameterMapper<Question>
 	 */
 	public String getIdColumnName() {
 		return "id";
+	}
+
+	@Override
+	public OrderedMapSqlParameterSource mapInsertSqlParams(final Question question) {
+		final OrderedMapSqlParameterSource orderedMapSqlParameterSource = new OrderedMapSqlParameterSource();
+		orderedMapSqlParameterSource.addValue("id", question.getId());
+		orderedMapSqlParameterSource.addValue("userId", question.getUserId());
+		orderedMapSqlParameterSource.addValue("question", question.getQuestion());
+		orderedMapSqlParameterSource.addValue("option_type", question.getOptionType());
+		orderedMapSqlParameterSource.addValue("qr_code_url", question.getQrCodeUrl());
+		orderedMapSqlParameterSource.addValue("media_url", question.getMediaUrl());
+		orderedMapSqlParameterSource.addValue("total_votes", question.getTotalVotes());
+		orderedMapSqlParameterSource.addValue("created_date", new Date());
+		orderedMapSqlParameterSource.addValue("modified_date", new Date());
+
+		return orderedMapSqlParameterSource;
 	}
 
 }
