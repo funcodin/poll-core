dbb8e2af4227607f491c4a7d960c1c0941210ebb
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-02-05 12:48:51 -0800

    Added remaining functionality for questions and options service

--- src/main/java/com/multi/enterprise/poll/core/service/QuestionService.java
+++ src/main/java/com/multi/enterprise/poll/core/service/QuestionService.java
@@ -4,6 +4,7 @@
 package com.multi.enterprise.poll.core.service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 import com.multi.enterprise.commons.service.BaseRecordService;
 import com.multi.enterprise.poll.core.dao.OptionsDao;
 import com.multi.enterprise.poll.core.dao.QuestionDao;
+import com.multi.enterprise.poll.core.validation.QuestionValidation;
+import com.multi.enterprise.types.exception.EntityNotFoundException;
 import com.multi.enterprise.types.exception.ServiceException;
 import com.multi.enterprise.types.poll.Options;
 import com.multi.enterprise.types.poll.Question;
@@ -41,25 +44,43 @@ public class QuestionService extends BaseRecordService<Question> {
 	@Override
 	public Question getById(String id) throws ServiceException {
 		final Question question = this.questionDao.getById(id);
-		List<Options> options = this.optionsDao.getAllOptionsByQuestionId(id);
+		final List<Options> options = this.optionsDao.getAllOptionsByQuestionId(id);
+		// Set total votes no need to store in DB
+		question.setTotalVotes(options.stream().mapToInt(option -> option.getVoteCount()).sum());
 		question.setOptions(options);
 		return question;
 	}
 
 	@Override
 	public Question create(final Question question) throws ServiceException {
+		QuestionValidation.isValidQuestion(question);
+
 		question.setId(UUID.randomUUID().toString());
 		question.setTotalVotes(0);
+		question.setOptionType(question.getOptionType().toUpperCase());
 		super.create(question);
 
 		for (final Options options : question.getOptions()) {
 			options.setId(UUID.randomUUID().toString());
 			options.setQuestionId(question.getId());
+			options.setOptionType(options.getOptionType().toUpperCase());
 			options.setVoteCount(0);
 			optionsDao.create(options);
 		}
-
 		return question;
 	}
 
+	@Override
+	public void delete(String id) throws ServiceException {
+		final Question question = this.questionDao.getById(id);
+
+		if (Objects.isNull(question)) {
+			throw new EntityNotFoundException(String.format(" Question not found for id %s", id));
+		}
+
+		for (final Options option : question.getOptions()) {
+			this.optionsDao.delete(option);
+		}
+		this.questionDao.delete(question);
+	}
 }
