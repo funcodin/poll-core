b3d8d49cc450d82fe04a511103a69f6e4bc462a4
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-02-05 18:55:21 -0800

    First cut for pagination

--- src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
+++ src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
@@ -3,10 +3,14 @@
  */
 package com.multi.enterprise.poll.core.dao;
 
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.multi.enterprise.commons.jdbc.dao.BaseJdbcRecordAccess;
 import com.multi.enterprise.types.poll.Question;
+import com.multi.enterprise.types.poll.QuestionList;
 
 /**
  * @author Robot
@@ -15,6 +19,8 @@ import com.multi.enterprise.types.poll.Question;
 @Repository
 public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 
+	private String SELECT_PAGINATED_QUESTION = "select * from question where id > %d order by id limit %d";
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -22,6 +28,26 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 	 */
 	public Class<Question> getDocumentClass() {
 		return Question.class;
+	}
+
+	public QuestionList getPaginatedQuestion(final int questionIndex, final int limit) {
+
+		final List<Question> questions = this.jdbcTempalte.query(SELECT_PAGINATED_QUESTION, this.rowMapper);
+		// TODO update this to compare against index
+		final Comparator<Question> comparator = (q1, q2) -> Integer.compare(q1.getTotalVotes(), q2.getTotalVotes());
+		final Question question = questions.stream().max(comparator).get();
+		final QuestionList questionList = new QuestionList();
+
+		questionList.setLastQuestionIndex(question.getTotalVotes());
+		questionList.setLimit(limit);
+
+		if (limit > questions.size()) {
+			questionList.setLastPage(true);
+		}
+		questionList.setQuestions(questions);
+
+		return questionList;
+
 	}
 
 }
---
+++ src/main/java/com/multi/enterprise/poll/core/service/QuestionListService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.multi.enterprise.poll.core.service;
+
+
+/**
+ * @author Robot
+ *
+ */
+public class QuestionListService {
+
+	public void getPaginatedQuestion(final int lastQuestionIndex, final String limit) {
+
+	}
+
+}
