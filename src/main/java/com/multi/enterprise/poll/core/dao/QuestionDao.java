1cbcb05e09be2c20c08e95c1b3cb4f1948369515
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-02-07 18:48:18 -0800

    Added latest question do function

--- src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
+++ src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
@@ -21,6 +21,8 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 
 	private String SELECT_PAGINATED_QUESTION = "select * from question where question_index > %d order by question_index limit %d";
 
+	private String SELECT_LATEST_PAGINATED_QUESTION = "SELECT * FROM question WHERE question_index > (SELECT MAX(question_index) - %d FROM question) order by question_index";
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -49,4 +51,25 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 		return questionList;
 
 	}
+
+	public QuestionList getLatestPaginatedQuestion(final int limit) {
+		final String query = String.format(SELECT_PAGINATED_QUESTION, limit);
+		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
+		final Comparator<Question> comparator = (q1, q2) -> Integer.compare(q1.getQuestionIndex(),
+				q2.getQuestionIndex());
+		final Question question = questions.stream().max(comparator).get();
+		final QuestionList questionList = new QuestionList();
+
+		questionList.setLastQuestionIndex(question.getQuestionIndex());
+		questionList.setLimit(limit);
+
+		if (limit > questions.size()) {
+			questionList.setLastPage(true);
+		}
+		questionList.setQuestions(questions);
+
+		return questionList;
+
+	}
+
 }
