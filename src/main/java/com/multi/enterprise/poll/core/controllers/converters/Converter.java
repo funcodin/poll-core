8b16cef323d2fa27f8414bf81cf615512ad842a9
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-03-12 00:13:09 -0800

    fix to make userid mandatory

--- src/main/java/com/multi/enterprise/poll/core/controllers/converters/Converter.java
+++ src/main/java/com/multi/enterprise/poll/core/controllers/converters/Converter.java
@@ -3,14 +3,16 @@
  */
 package com.multi.enterprise.poll.core.controllers.converters;
 
+import com.multi.enterprise.types.exception.ServiceException;
+
 /**
  * @author Robot
  *
  */
 public interface Converter<R, T> {
 
-	public R internalize(final T entity);
+	public R internalize(final T entity) throws ServiceException;
 
-	public T externalize(final R entity);
+	public T externalize(final R entity) throws ServiceException;
 
 }
