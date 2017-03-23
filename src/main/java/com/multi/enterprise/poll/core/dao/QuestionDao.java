04306ea5361a9c4e83f9d2819f5b2b1cc6e5af22
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-03-22 20:43:35 -0700

    Added functionality to get paginated question for user

--- src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
+++ src/main/java/com/multi/enterprise/poll/core/dao/QuestionDao.java
@@ -23,6 +23,14 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 
 	private String SELECT_LATEST_PAGINATED_QUESTION = "SELECT * FROM question order by question_index desc limit %d";
 
+	private String SELECT_LASTEST_PAGINATED_QUESTION_FOR_USER = "select * from question where id not in"
+			+ " ( select distinct(question_id) from user_poll where user_id = '%s' ) "
+			+ "and userId <> '%s' order by question_index desc limit %d";
+
+	private String SELECT_PAGINATED_QUESTION_FOR_USER = "select * from question where id not in"
+			+ " ( select distinct(question_id) from user_poll where user_id = '%s' ) "
+			+ "and userId <> '%s' and question_index < %d order by question_index desc limit %d";
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -35,6 +43,32 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 	public QuestionList getPaginatedQuestion(final int questionIndex, final int limit) {
 		final String query = String.format(SELECT_PAGINATED_QUESTION, questionIndex, limit);
 		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
+		return this.convertToQuestionList(questions, limit);
+
+	}
+
+	public QuestionList getLatestPaginatedQuestion(final int limit) {
+		final String query = String.format(SELECT_LATEST_PAGINATED_QUESTION, limit);
+		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
+		return this.convertToQuestionList(questions, limit);
+
+	}
+
+	public QuestionList getLatestPaginatedQuestionForUser(final String userId, final int limit) {
+		final String query = String.format(SELECT_LASTEST_PAGINATED_QUESTION_FOR_USER, userId, userId, limit);
+		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
+		return this.convertToQuestionList(questions, limit);
+
+	}
+
+	public QuestionList getPaginatedQuestionForUser(final String userId, final int questionIndex, final int limit) {
+		final String query = String.format(SELECT_PAGINATED_QUESTION_FOR_USER, userId, userId, questionIndex, limit);
+		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
+		return this.convertToQuestionList(questions, limit);
+
+	}
+
+	private QuestionList convertToQuestionList(final List<Question> questions, final int limit) {
 		final Comparator<Question> comparator = (q1, q2) -> Integer.compare(q1.getQuestionIndex(),
 				q2.getQuestionIndex());
 		final Question question = questions.stream().min(comparator).get();
@@ -49,27 +83,6 @@ public class QuestionDao extends BaseJdbcRecordAccess<Question> {
 		questionList.setQuestions(questions);
 
 		return questionList;
-
-	}
-
-	public QuestionList getLatestPaginatedQuestion(final int limit) {
-		final String query = String.format(SELECT_LATEST_PAGINATED_QUESTION, limit);
-		final List<Question> questions = this.jdbcTempalte.query(query, this.rowMapper);
-		final Comparator<Question> comparator = (q1, q2) -> Integer.compare(q1.getQuestionIndex(),
-				q2.getQuestionIndex());
-		final Question question = questions.stream().min(comparator).get();
-		final QuestionList questionList = new QuestionList();
-
-		questionList.setLastQuestionIndex(question.getQuestionIndex());
-		questionList.setLimit(limit);
-
-		if (limit > questions.size()) {
-			questionList.setLastPage(true);
-		}
-		questionList.setQuestions(questions);
-
-		return questionList;
-
 	}
 
 }
