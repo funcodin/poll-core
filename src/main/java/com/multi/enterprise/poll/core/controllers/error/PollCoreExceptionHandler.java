157e4322689da708d815304742e5a4246157e2b1
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-06-06 20:28:35 -0700

    Return status

--- src/main/java/com/multi/enterprise/poll/core/controllers/error/PollCoreExceptionHandler.java
+++ src/main/java/com/multi/enterprise/poll/core/controllers/error/PollCoreExceptionHandler.java
@@ -3,9 +3,11 @@
  */
 package com.multi.enterprise.poll.core.controllers.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.multi.enterprise.types.error.ResponseException;
 import com.multi.enterprise.types.exception.IllegalArgumentServiceException;
@@ -19,6 +21,7 @@ import com.multi.enterprise.types.exception.IllegalArgumentServiceException;
 public class PollCoreExceptionHandler {
 
 	@ExceptionHandler(IllegalArgumentServiceException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public @ResponseBody ResponseException handleIllegalArgumentServiceException(
 			final IllegalArgumentServiceException illegalArgumentServiceException) {
 		final ResponseException responseException = new ResponseException();
