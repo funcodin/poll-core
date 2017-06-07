2e2a81754141399f7791c80022683e273ebc8806
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-06-06 23:14:16 -0700

    Added some more methods to controller advice

--- src/main/java/com/multi/enterprise/poll/core/controllers/error/PollCoreExceptionHandler.java
+++ src/main/java/com/multi/enterprise/poll/core/controllers/error/PollCoreExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.multi.enterprise.types.error.ResponseException;
 import com.multi.enterprise.types.exception.IllegalArgumentServiceException;
+import com.multi.enterprise.types.exception.ServiceException;
 
 /**
  * @author Robot
@@ -27,6 +28,15 @@ public class PollCoreExceptionHandler {
 		final ResponseException responseException = new ResponseException();
 		responseException.setErrorMessage(illegalArgumentServiceException.getMessage());
 		responseException.setStatusCode(illegalArgumentServiceException.getHttpStatus());
+		return responseException;
+	}
+
+	@ExceptionHandler(ServiceException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public @ResponseBody ResponseException handleServiceException(final ServiceException serviceException) {
+		final ResponseException responseException = new ResponseException();
+		responseException.setErrorMessage(serviceException.getMessage());
+		responseException.setStatusCode(serviceException.getHttpStatus());
 		return responseException;
 	}
 
